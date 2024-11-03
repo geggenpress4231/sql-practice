892493733a800b16e76c2fcccb42ca4a8e96f83d
geggenpress4231 <ami.utsarabbani@gmail.com>
2024-11-03 00:32:07 -0400

    thrd

--- ConfirmationRate.sql
+++ ConfirmationRate.sql
@@ -11,3 +11,14 @@ SELECT s.user_id,
 FROM Signups s
 LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
+
+--optimised
+
+SELECT s.user_id, 
+       COALESCE(
+           ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.user_id), 2),
+           0
+       ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
